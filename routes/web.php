353aa91b4dbc56c4de3068498de97028cfcc6359
brighamdr <35384086+brighamdr@users.noreply.github.com>
2020-10-19 12:23:27 -0500

    Pruebas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', function () {
+    return 'Usuarios';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "Mostrando detalle de usuario: {$id}";
+});
+
---
+++ tests/Feature/UsersModuleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UsersModuleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_if_loads_the_users_list()
+    {
+        $this->get('/usuarios')
+        ->assertStatus(200)
+        ->assertSee('Usuarios');
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_if_loads_the_users_details_page()
+    {
+        $this->get('/usuarios/5')
+        ->assertStatus(200)
+        ->assertSee('Mostrando detalle de usuario: 5');
+    }
+
+
+
+}
